8663f0639bab1e85dc78639135b6ebb9c5d5784b
faizan517 <faizanpervaiz517@gmail.com>
2024-10-15 10:31:57 +0500

    working-on-dark-disableing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 document.body.style = 'background: white;';
 import React, { Suspense, useEffect } from 'react'
 import { HashRouter, Route, Routes } from 'react-router-dom'
-import { useSelector } from 'react-redux'
 
-import { CSpinner, useColorModes } from '@coreui/react'
+import { CSpinner} from '@coreui/react'
 import './scss/style.scss'
 import Color from './utils/Color';
 
@@ -17,24 +16,8 @@ const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 
 const App = () => {
-  const { isColorModeSet, setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
-  const storedTheme = useSelector((state) => state.theme)
-
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.href.split('?')[1])
-    const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0]
-    if (theme) {
-      setColorMode(theme)
-    }
-
-    if (isColorModeSet()) {
-      return
-    }
-
-    setColorMode(storedTheme)
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps
-
-  return (
+    return (
+      
     <HashRouter>
       <Suspense
         fallback={
